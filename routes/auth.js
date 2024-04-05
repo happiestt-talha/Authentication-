a649125846ce1814b24257552146b5242d9dea3d
Talha <mtalhamanzoor786@gmail.com>
2024-04-05 23:30:51 +0500

    somehow done it. took some AI for help. but still not getting the required results cuz i messed up in start at folder structuring, shouldn't have made seprate folders :(

--- routes/auth.js
+++ routes/auth.js
@@ -1,52 +1,56 @@
-require('dotenv').config()
-const express=require('express')
-const jwt=require('jsonwebtoken')
-const app=express();
-const posts=[
-    {
-        username:'talha',
-        msg:'hi talha'
-    },
-    {
-        username:'ali',
-        msg:'hi ali'
-    },
-    {
-        username:'ahmad',
-        msg:'hi ahmad'
-    },
-    {
-        username:'noone',
-        msg:'hi noone'
+require('dotenv').config();
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const app = express();
+const posts = [
+    { username: 'talha', msg: 'hi talha' },
+    { username: 'ali', msg: 'hi ali' },
+    { username: 'ahmad', msg: 'hi ahmad' },
+    { username: 'noone', msg: 'hi noone' }
+];
+
+// Middleware to authenticate JWT token
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized: Token is required' });
     }
-]
-app.post('/',authenticateToken,(req,res)=>{
-    const username=req.body.username;
 
-    //? serialize user with using a secret key REQUIRED
-    const user={name:username}
-
-        //?sign the created user
-    const accessToken= jwt.sign(user,process.env.PRIVATE_ACCESS_TOKEN)
-    res.json(user)
-    
-})
-
-function authenticateToken(req,res,next){
-
-    //! get and verify existance the token from users request
-    const authHeader=req.headers['authorization'];
-    const token=authHeader?.split(' ')[1];
-    if(token===null) res.sendStatus(401)
-
-    //! now we know user have a token
-    jwt.verify(token,process.env.PRIVATE_ACCESS_TOKEN,(err,user)=>{
-        if(err){
-            return res.sendStatus(403)
+    jwt.verify(token, process.env.PRIVATE_ACCESS_TOKEN, (err, user) => {
+        if (err) {
+            console.error('Token verification error:', err);
+            return res.status(403).json({ error: 'Forbidden: Invalid token' });
         }
-        req.user=user;
+        req.user = user;
         next();
-    })
-    
+    });
 }
-module.exports=app
+
+// Route to generate JWT token
+app.post('/', (req, res) => {
+    const { username } = req.body;
+
+    if (!username) {
+        return res.status(400).json({ error: 'Username is required' });
+    }
+
+    const user = { name: username };
+    const accessToken = jwt.sign(user, process.env.PRIVATE_ACCESS_TOKEN);
+    res.json({ accessToken: accessToken });
+});
+
+// Protected route to get posts
+app.get('/possts', authenticateToken, (req, res) => {
+    console.log(req.user);
+    res.json(posts);
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+module.exports = app;
