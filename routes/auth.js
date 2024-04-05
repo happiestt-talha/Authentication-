ee834b01c62e90249e46f19faff59c216eeadbda
Talha <mtalhamanzoor786@gmail.com>
2024-04-05 20:35:10 +0500

    kisi had tak JWT token done ha, full dakhny k liy next commit check kijiy

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,25 @@ require('dotenv').config()
 const express=require('express')
 const jwt=require('jsonwebtoken')
 const app=express();
-
-app.post('/',(req,res)=>{
+const posts=[
+    {
+        username:'talha',
+        msg:'hi talha'
+    },
+    {
+        username:'ali',
+        msg:'hi ali'
+    },
+    {
+        username:'ahmad',
+        msg:'hi ahmad'
+    },
+    {
+        username:'noone',
+        msg:'hi noone'
+    }
+]
+app.post('/',authenticateToken,(req,res)=>{
     const username=req.body.username;
 
     //? serialize user with using a secret key REQUIRED
@@ -11,8 +28,25 @@ app.post('/',(req,res)=>{
 
         //?sign the created user
     const accessToken= jwt.sign(user,process.env.PRIVATE_ACCESS_TOKEN)
-    res.json({accessToken:accessToken})
-// res.json({name:user.name})
+    res.json(user)
+    
 })
 
+function authenticateToken(req,res,next){
+
+    //! get and verify existance the token from users request
+    const authHeader=req.headers['authorization'];
+    const token=authHeader?.split(' ')[1];
+    if(token===null) res.sendStatus(401)
+
+    //! now we know user have a token
+    jwt.verify(token,process.env.PRIVATE_ACCESS_TOKEN,(err,user)=>{
+        if(err){
+            return res.sendStatus(403)
+        }
+        req.user=user;
+        next();
+    })
+    
+}
 module.exports=app
